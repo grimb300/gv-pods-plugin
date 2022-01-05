d4b2538f42a5180cbc941f5e614235b1096229e2
Bob Grim <grimb300@hotmail.com>
2022-01-05 11:43:48 -0800

    add rudimentary display function to phone_numbers field type

--- classes/fields/gv-phone-numbers.php
+++ classes/fields/gv-phone-numbers.php
@@ -50,4 +50,31 @@ class PodsField_GV_Phone_Numbers extends PodsField {
 
     pods_view( $view, compact( array_keys( get_defined_vars() ) ) );
   }
+
+  // Point Pods to the custom front end display view for this field type
+  // NOTE: This is used by the Pods Field Value block and I suspect magic tags.
+  //       I shouldn't care too much about this if I create a custom block for GV.
+  public function display( $value = null, $name = null, $options = null, $pod = null, $id = null ) {
+
+    $value_raw = $value;
+    // NOTE: This appears to get called for each subfield (number_0/1/2 and description_0/1/2)
+    // return '<p>This is the GV Phone Numbers raw value (' . $name . '):</p><code>' . $value_raw . '</code>';
+
+    // Get the max count of phone numbers out of the options
+    $max_phone_numbers = pods_v( self::$type . '_count', $options, '3' );
+
+    $view = GV_PLUGIN_PATH . 'ui/front/gv-phone-numbers.php';
+    // TODO: pods-address-field example adds a a filter here to allow other plugins to change the view
+    
+    $output = pods_view( $view, compact( array_keys( get_defined_vars() ) ), false, 'cache', true );
+    // TODO: pods-address-field example adds a a filter here to allow other plugins to change the output
+
+    return $output;
+  }
+
+  // I think this override may be necessary to force one "display" call on the entire field
+  // instead of a "display" call on each element of the field (number_0/1/2 and description_0/1/2)
+  public function display_list( $value = null, $name = null, $options = null, $pod = null, $id = null ) {
+    return call_user_func_array( array( $this, 'display' ), func_get_args() );
+  }
 }
--- ui/front/gv-phone-numbers.php
+++ ui/front/gv-phone-numbers.php
@@ -1,7 +1,31 @@
 <?php
+error_log( 'Value is' );
+error_log( var_export( $value, true ) );
 
-if ( ! empty( $value[ 'gv_phone_numbers' ] ) ) {
-  $format = PodsForm::field_method( 'gv_phone_numbers', 'default_display_format' );
+// TODO: pods-address-field example has a "default format" and a user defined "custom format"
+//       It then runs the appropriate format through a preg/str_replace to turn that into html.
+//       I might want to do something similar at a later time depending on GV needs.
 
-  if ( 'custom' === pods_v( '')){}
+// Get the max number of phone numbers and make sure it is an integer
+if ( empty( $max_phone_numbers ) ) {
+  $max_phone_numbers = pods_v( 'gv_phone_numbers_count', $options, '3' );
 }
+$max_phone_numbers = intval( $max_phone_numbers );
+
+// TODO: Long term I could support multiple format methods (thinking unordered list, grid, table, etc)
+?>
+<h5>Phone Numbers</h5>
+<ul>
+  <?php for ( $i = 0; $i < $max_phone_numbers; $i++ ) { ?>
+    <?php if ( ! empty( $value[ 'number_' . $i ] ) ) { ?>
+      <li>
+        <ul>
+          <li><?php echo $value[ 'number_' . $i ]; ?></li>
+          <?php if ( ! empty( $value[ 'description_' . $i ] ) ) { ?>
+            <li><?php echo $value[ 'description_' . $i ]; ?></li>
+          <?php } ?>
+        </ul>
+      </li>
+    <?php } ?>
+  <?php } ?>
+</ul>
