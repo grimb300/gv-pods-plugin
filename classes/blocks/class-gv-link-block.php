16a3ad13351074c39eab976755c4e6817692f831
Bob Grim <grimb300@hotmail.com>
2022-02-08 15:49:38 -0800

    complete link block (url and twitter blocks)

--- classes/blocks/class-gv-link-block.php
+++ classes/blocks/class-gv-link-block.php
@@ -17,16 +17,23 @@ class GV_Link_Block extends GV_Default_Block {
 
   // Customized constructor
   public function __construct( $params = array() ) {
-    gv_debug( 'Executing the parent constructor from the link block' );
     parent::__construct( $params );
 
     // Add attribute fields specific to a link block
-    $this->attributes[] = array(
-      'name' => 'link_text',
-      'label' => 'Link Text',
-      'type' => 'text',
-      'placeholder' => 'placeholder text'
+    $link_block_attributes = array(
+      array(
+        'name' => 'link_text',
+        'label' => 'Link Text',
+        'type' => 'text',
+        'description' => 'Leaving this blank will display the URL',
+      ),
+      array(
+        'name' => 'new_window',
+        'label' => 'Open Link in New Window',
+        'type' => 'boolean',
+      ),
     );
+    $this->attributes = array_merge( $this->attributes, $link_block_attributes );
   }
 
   // Display a link field
@@ -36,12 +43,12 @@ class GV_Link_Block extends GV_Default_Block {
     // If field data is empty, return nothing
     if ( empty( $field_data ) ) return;
 
+    $url = 'twitter_username' === $this->field_name ? sprintf( 'https://twitter.com/%s', $field_data ) : $field_data;
+    $link_text = empty( $attributes[ 'link_text' ] ) ? $field_data : $attributes[ 'link_text' ];
+    $target = ! empty( $attributes[ 'new_window' ] ) && $attributes[ 'new_window' ] ? ' target="_blank"' : '';
+
     // Create the link, if no link text provided use the field data
-    $formatted_field_data = sprintf(
-      '<a href="%s">%s</a>',
-      $field_data,
-      empty( $attributes[ 'link_text' ] ) ? $field_data : $attributes[ 'link_text' ]
-    );
+    $formatted_field_data = sprintf( '<a href="%s"%s>%s</a>', $url, $target, $link_text );
 
     // Return the formatted field data
     return $formatted_field_data;
