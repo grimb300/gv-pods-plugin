21bae313c877953ac8efb3654e08e3cc1e506495
Bob Grim <grimb300@hotmail.com>
2022-02-10 16:36:44 -0800

    add text styles to phone numbers and descriptions

--- classes/blocks/class-gv-phone-numbers-block.php
+++ classes/blocks/class-gv-phone-numbers-block.php
@@ -18,8 +18,23 @@ class GV_Phone_Numbers_Block extends GV_Default_Block {
    * Methods
    * *******/
 
-  // Display a location field
-  protected function format_field_data( $field_data = null ) {
+  // Customized constructor
+  public function __construct( $params = array() ) {
+    parent::__construct( $params );
+
+    // Give two sets of text styles, one for the phone number an another for the description
+    $this->attributes = array_merge(
+      $this->attributes,
+      $this->generate_text_style_fields( 'num', 'Phone Number' ),
+      $this->generate_text_style_fields( 'dsc', 'Description' )
+    );
+  }
+
+  // Display the field
+  protected function format_field_data( $field_data = null, $attributes = array() ) {
+    // If field data is empty, return nothing
+    if ( empty( $field_data ) ) return;
+
     $formatted_data = '
       <style>
         .gv_phone_numbers_wrap {
@@ -28,6 +43,17 @@ class GV_Phone_Numbers_Block extends GV_Default_Block {
           column-gap: 10px;
           row-gap: 10px;
         }
+    ';
+    // Get the text styles for the phone number and description
+    $num_styles = $this->generate_text_style_attributes( $attributes, 'num' );
+    if ( ! empty( $num_styles ) ) {
+      $formatted_data .= sprintf( '.gv_phone_numbers_number{%s;}', implode( ';', $num_styles ) );
+    }
+    $dsc_styles = $this->generate_text_style_attributes( $attributes, 'dsc' );
+    if ( ! empty( $dsc_styles ) ) {
+      $formatted_data .= sprintf( '.gv_phone_numbers_description{%s;}', implode( ';', $dsc_styles ) );
+    }
+    $formatted_data .= '
       </style>
       <div class="gv_phone_numbers_wrap">
         <div class="gv_phone_numbers_number">Phone Number</div>
