e3f8e7b4cac84fc50a6681c7b80e30910eb4afcb
Bob Grim <grimb300@hotmail.com>
2022-02-14 15:45:18 -0800

    quick and dirty map display

--- classes/blocks/class-gv-location-block.php
+++ classes/blocks/class-gv-location-block.php
@@ -20,6 +20,54 @@ class GV_Location_Block extends GV_Default_Block {
 
   // Display the field
   protected function format_field_data( $field_data = null, $attributes = array() ) {
+    // global $post;
+    // $pod = pods( $this->post_type, $post->ID );
+    // $return_string = $pod->display( $this->field_name );
+    // gv_debug( $return_string );
+    // return $return_string;
+    // wp_enqueue_script( 'googlemaps' );
+    // wp_enqueue_script( 'pods-maps' );
+    // wp_enqueue_style( 'pods-maps' );
+
+    gv_debug( 'map field data' );
+    gv_debug( $field_data );
+
+    // TODO: This is a quick and dirty way to display the current business on a map.
+    //       Eventually upgrade this to have markers for other businesses and parameterize it better.
+    //       Someday it might even use the pods map type. :)
+    $field_data = '
+    <style>
+    /* Set the size of the div element that contains the map */
+    #map {
+      height: 400px;
+      width: 100%;
+    }
+    </style>
+    <script>
+    // Initialize and add teh map
+    function initMap() {
+      // The location of the business
+      const business = { lat: ' . $field_data['geo']['lat'] . ', lng: ' . $field_data['geo']['lng'] . ' };
+      // The map, centered at Uluru
+      const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 12,
+        center: business,
+      });
+      // The marker, positioned at the business
+      const marker = new google.maps.Marker({
+        position: business,
+        map: map,
+      });
+    }
+    </script>
+    <div id="map"></div>
+    <script
+      src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXmME5HmoMq1K85S2Z7kVksAuPInsrHhM&callback=initMap&libraries=&v=weekly"
+      async
+    ></script>
+    ';
+
+    return $field_data;
     return '<p style="color: red"><strong><em>don\'t know how to display a location field yet.</em></strong></p>';
   }
 }
